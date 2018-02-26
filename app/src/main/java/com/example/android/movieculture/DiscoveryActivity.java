bd8c9e0d7e1474a8493c32575413ec4acf7cc48e
WebDevIon <31899558+WebDevIon@users.noreply.github.com>
2018-02-26 11:39:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/movieculture/DiscoveryActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.movieculture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiscoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.discovery_activity);
+    }
+}
