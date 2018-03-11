c04246f42d6795633c757243bc64cd2d408ee5a9
Alin Simon <simon.alin90@gmail.com>
2018-03-11 21:47:43 +0200

    Updated the Discover Activity to hold a Grid View and a menu button.

--- app/src/main/java/com/example/android/movieculture/DiscoveryActivity.java
+++ app/src/main/java/com/example/android/movieculture/DiscoveryActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.movieculture;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class DiscoveryActivity extends AppCompatActivity {
 
@@ -9,5 +13,56 @@ public class DiscoveryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.discovery_activity);
+    }
+
+    /**
+     * This is where we inflate and set up the menu for this Activity.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @return You must return true for the menu to be displayed;
+     *         if you return false it will not be shown.
+     *
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Here we create a instance of the Menu Inflater
+        MenuInflater inflater = getMenuInflater();
+
+        // We inflate the menu
+        inflater.inflate(R.menu.main_menu, menu);
+
+        // We return true so that the menu is shown
+        return true;
+    }
+
+    /**
+     * Callback invoked when a menu item was selected from this Activity's menu.
+     *
+     * @param item The menu item that was selected by the user
+     *
+     * @return true if you handle the menu click here, false otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Here we store the id of the item that was selected by the user
+        int id = item.getItemId();
+
+        // If the id matches that of the About menu item then we launch the About Activity
+        // TODO: Create an activity fragment for the About Activity.
+        if (id == R.id.action_about) {
+            Toast.makeText(this, "About menu pressed", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        // If the id matches that of the Settings menu item then we launch the Menu Activity
+        // TODO: Create an activity fragment for the Menu Activity.
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings menu pressed", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
